837ea0aea41678f7d63fdf9cc8e894e7854f970c
Nouzan <clobraindie@outlook.com>
2019-10-30 10:37:28 +0800

    remove ownership chain

--- dsa/lesson-linear/src/list/node/flat_node.rs
+++ dsa/lesson-linear/src/list/node/flat_node.rs
@@ -8,26 +8,22 @@ use crate::node::Node;
 #[derive(Debug)]
 pub struct RawNode<T> {
     data: Option<Node<T>>,
-    pred: Weak<RefCell<RawNode<T>>>,
+    pred: Option<Rc<RefCell<RawNode<T>>>>,
     suss: Weak<RefCell<RawNode<T>>>,
-    next: Option<Rc<RefCell<RawNode<T>>>>,
-    owner: Weak<RefCell<RawNode<T>>>,
 }
 
 impl<T> RawNode<T> {
     pub fn new(data: T) -> Self {
         RawNode {
             data: Some(Node::new(data)),
-            pred: Weak::new(),
+            pred: None,
             suss: Weak::new(),
-            next: None,
-            owner: Weak::new(),
         }
     }
 
     fn pred(&self) -> Option<ListNode<T>> {
-        if let Some(node) = self.pred.upgrade() {
-            Some(ListNode(node.clone()))
+        if let Some(node) = self.pred.clone() {
+            Some(ListNode(node))
         } else {
             None
         }
@@ -35,22 +31,6 @@ impl<T> RawNode<T> {
 
     fn suss(&self) -> Option<ListNode<T>> {
         if let Some(node) = self.suss.upgrade() {
-            Some(ListNode(node.clone()))
-        } else {
-            None
-        }
-    }
-
-    fn next(&self) -> Option<ListNode<T>> {
-        if let Some(node) = self.next.clone() {
-            Some(ListNode(node))
-        } else {
-            None
-        }
-    }
-
-    fn owner(&self) -> Option<ListNode<T>> {
-        if let Some(node) = self.owner.upgrade() {
             Some(ListNode(node.clone()))
         } else {
             None
@@ -77,30 +57,12 @@ impl<T: std::fmt::Debug> ListNode<T> {
 
     pub fn insert_as_pred(&self, data: T) -> Self {
         let pred = Self::new(data);
-
-        // 维护ownership链
-        if let Some(next) = self.next() {
-            pred.0.borrow_mut().next = Some(next.0.clone());
-            next.0.borrow_mut().owner = Rc::downgrade(&pred.0);
-        }
-        self.0.borrow_mut().next = Some(pred.0.clone());
-        pred.0.borrow_mut().owner = Rc::downgrade(&self.0);
-
         self.link_before(&pred);
         pred
     }
 
     pub fn insert_as_suss(&self, data: T) -> Self {
         let suss = Self::new(data);
-
-        // 维护ownership链
-        if let Some(next) = self.next() {
-            suss.0.borrow_mut().next = Some(next.0.clone());
-            next.0.borrow_mut().owner = Rc::downgrade(&suss.0);
-        }
-        self.0.borrow_mut().next = Some(suss.0.clone());
-        suss.0.borrow_mut().owner = Rc::downgrade(&self.0);
-
         self.link_after(&suss);
         suss
     }
@@ -117,6 +79,11 @@ impl<T: std::fmt::Debug> ListNode<T> {
         unsafe {
             ptr.as_ref().unwrap().get()
         }
+    }
+
+    pub fn set_node(&self, node: Node<T>) -> &Self {
+        self.0.borrow_mut().data = Some(node);
+        self
     }
 
     pub fn remove_node(&self) -> Option<Node<T>> {
@@ -141,62 +108,37 @@ impl<T: std::fmt::Debug> ListNode<T> {
         self.0.borrow().suss()
     }
 
-    fn next(&self) -> Option<ListNode<T>> {
-        self.0.borrow().next()
-    }
-
-    fn owner(&self) -> Option<ListNode<T>> {
-        self.0.borrow().owner()
-    }
-
-    pub fn remove_ownership(&self) -> &Self {
-        if let Some(p) = self.owner() {
-            p.0.borrow_mut().next = self.0.borrow_mut().next.clone();
-            if let Some(q) = self.next() {
-                q.0.borrow_mut().owner = Rc::downgrade(&p.0);
-            }
-        }
-        self.0.borrow_mut().owner = Weak::new();
-        self.0.borrow_mut().next = None;
-
-        self
-    }
-
-    pub fn insert_ownership(&self, p: &Self) -> &Self {
-        if !Rc::ptr_eq(&self.0, &p.0) {
-            p.0.borrow_mut().owner = Rc::downgrade(&self.0);
-            p.0.borrow_mut().next = self.0.borrow_mut().next.clone();
-            if let Some(q) = self.next() {
-                q.0.borrow_mut().owner = Rc::downgrade(&p.0);
-            }
-            self.0.borrow_mut().next = Some(p.0.clone());
-        }
-        self
+    fn check_alone(&self, p: &Self) -> bool {
+        Rc::ptr_eq(&self.0, &p.0) || p.pred().is_some() || p.suss().is_some()
     }
 
     pub fn link_after(&self, p: &Self) -> &Self {
-        p.0.borrow_mut().suss = self.0.borrow_mut().suss.clone();
-        if let Some(q) = self.suss() {
-            q.0.borrow_mut().pred = Rc::downgrade(&p.0);
+        if !self.check_alone(p) {
+            p.0.borrow_mut().suss = self.0.borrow_mut().suss.clone();
+            if let Some(q) = self.suss() {
+                q.0.borrow_mut().pred = Some(Rc::clone(&p.0));
+            }
+            self.0.borrow_mut().suss = Rc::downgrade(&p.0);
+            p.0.borrow_mut().pred = Some(Rc::clone(&self.0));
         }
-        self.0.borrow_mut().suss = Rc::downgrade(&p.0);
-        p.0.borrow_mut().pred = Rc::downgrade(&self.0);
         self
     }
 
     pub fn link_before(&self, p: &Self) -> &Self {
-        p.0.borrow_mut().pred = self.0.borrow_mut().pred.clone();
-        if let Some(q) = self.pred() {
-            q.0.borrow_mut().suss = Rc::downgrade(&p.0);
+        if !self.check_alone(p) {
+            p.0.borrow_mut().pred = self.0.borrow_mut().pred.clone();
+            if let Some(q) = self.pred() {
+                q.0.borrow_mut().suss = Rc::downgrade(&p.0);
+            }
+            self.0.borrow_mut().pred = Some(Rc::clone(&p.0));
+            p.0.borrow_mut().suss = Rc::downgrade(&self.0);
         }
-        self.0.borrow_mut().pred = Rc::downgrade(&p.0);
-        p.0.borrow_mut().suss = Rc::downgrade(&self.0);
         self
     }
 
     pub fn clear_after(&self) -> &Self {
         if let Some(q) = self.suss() {
-            q.0.borrow_mut().pred = Weak::new();
+            q.0.borrow_mut().pred = None;
         }
         self.0.borrow_mut().suss = Weak::new();
         self
@@ -206,7 +148,7 @@ impl<T: std::fmt::Debug> ListNode<T> {
         if let Some(p) = self.pred() {
             p.0.borrow_mut().suss = Weak::new();
         }
-        self.0.borrow_mut().pred = Weak::new();
+        self.0.borrow_mut().pred = None;
         self
     }
 
@@ -216,7 +158,7 @@ use std::fmt;
 
 impl<T: fmt::Debug> fmt::Debug for ListNode<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut res = write!(f, "List: ");
+        let mut res = write!(f, "");
         let mut p = self.suss();
         let mut q = self.pred();
         let mut preds: Vec<String> = vec![];
@@ -240,31 +182,6 @@ impl<T: fmt::Debug> fmt::Debug for ListNode<T> {
             res = write!(f, " -> {}", rc.print_node());
             p = rc.suss();
         }
-        res = write!(f, " ");
-        res = write!(f, "`Ownership: ");
-
-        let mut mp = self.owner();
-        let mut owners: Vec<String> = vec![];
-
-        while let Some(p) = mp {
-            owners.push(format!("{}", p.print_node()));
-            mp = p.owner();
-        }
-
-        for owner in owners.iter().rev() {
-            res = write!(f, "{} -> ", owner);
-        }
-
-        res = write!(f, "(*){}", self.print_node());
-
-        let mut mp = self.next();
-
-        while let Some(p) = mp {
-            res = write!(f, " -> {}", p.print_node());
-            mp = p.next();
-        }
-
-        res = write!(f, "`");
         res
     }
 }
--- dsa/main/src/main.rs
+++ dsa/main/src/main.rs
@@ -10,21 +10,33 @@ use std::rc::Rc;
 use std::cell::RefCell;
 
 fn main() {
+    // let a = ListNode::new(1);
+    // a.insert_as_suss(2);
+    // a.insert_as_suss(3);
+    // a.insert_as_pred(4);
+    // a.suss().unwrap().insert_as_pred(5);
+    // let b = a.suss().unwrap();
+    // a.suss().unwrap().remove_ownership();
+    // a.clear_after();
+    // // a.remove_ownership();
+
+    // println!("{:?}", a);
+    // println!("{:?}", b);
+
+    // a.insert_ownership(&b);
+
+    // println!("{:?}", a);
+    // println!("{:?}", b);
     let a = ListNode::new(1);
-    a.insert_as_suss(2);
-    a.insert_as_suss(3);
-    a.insert_as_pred(4);
-    a.suss().unwrap().insert_as_pred(5);
-    let b = a.suss().unwrap();
-    a.suss().unwrap().remove_ownership();
-    a.clear_after();
-    // a.remove_ownership();
-
-    println!("{:?}", a);
-    println!("{:?}", b);
-
-    a.insert_ownership(&b);
-
-    println!("{:?}", a);
-    println!("{:?}", b);
+    let b = ListNode::new(2);
+    // a.insert_ownership(&b);
+    // b.insert_ownership(&a);
+    println!("{:?} {:?}", a, b);
+    a.link_after(&b);
+    println!("{:?} {:?}", a, b);
+    a.insert_as_pred(3);
+    let n = a.remove_node();
+    println!("{:?} {:?}", a, n);
+    a.set_node(n.unwrap());
+    println!("{:?} {:?}", a, a.get_node());
 }
